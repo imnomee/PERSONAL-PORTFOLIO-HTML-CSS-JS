29adc2be4bff5f1afb9c42c68ba71f6f2b5e7a67
Noman Rafiq <heyimnomee@gmail.com>
2024-09-10 13:57:25 +0500

    navbar blur

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ function openMenu() {
 function closeMenu() {
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+const navBar = document.querySelector('nav');
+const linksList = document.querySelector('nav ul');
+window.addEventListener('scroll', () => {
+    if (scrollY > 50) {
+        navBar.classList.add(
+            `bg-white`,
+            'bg-opacity-50',
+            'backdrop-blur-lg',
+            'shadow-md',
+            'duration-500'
+        );
+        linksList.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    } else {
+        navBar.classList.remove(
+            'bg-white',
+            'bg-opacity-50',
+            'backdrop-blur-lg',
+            'shadow-md',
+            'duration-500'
+        );
+        linksList.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+});
